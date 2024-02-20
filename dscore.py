9fbaf8cf344a97e858d776ef54e6c461d5381786
Shota Horiguchi <hshota0530@gmail.com>
2024-02-21 00:47:31 +0900

    [update] enable import dscore

--- dscore.py
+++ dscore.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
 
-from score import check_for_empty_files
-from score import load_rttms
-from scorelib.turn import merge_turns, trim_turns
-from scorelib.uem import gen_uem, load_uem
-from scorelib.score import score
+from .score import check_for_empty_files
+from .score import load_rttms
+from dscore.scorelib.turn import merge_turns, trim_turns
+from dscore.scorelib.uem import gen_uem, load_uem
+from dscore.scorelib.score import score
 
 
 def calc_der(ref_rttm, sys_rttm, uem=None,
              collar=0.0, ignore_overlaps=False):
     ref_turns, _ = load_rttms(ref_rttm)
     sys_turns, _ = load_rttms(sys_rttm)
-
-    args.ref_rttm_fns = load_script_file(ref_rttm)
-    args.sys_rttm_fns = load_script_file(sys_rttm)
 
     if uem is not None:
         uem = load_uem(uems)
@@ -28,13 +25,10 @@ def calc_der(ref_rttm, sys_rttm, uem=None,
     # Score.
     check_for_empty_files(ref_turns, sys_turns, uem)
     file_scores, global_scores = score(
-        ref_turns, sys_turns, uem, step=args.step,
-        jer_min_ref_dur=args.jer_min_ref_dur, collar=args.collar,
-        ignore_overlaps=args.ignore_overlaps)
+        ref_turns, sys_turns, uem, step=0.010,
+        jer_min_ref_dur=0.0, collar=collar,
+        ignore_overlaps=ignore_overlaps)
 
-    return {
-        'der': global_scores['der'],
-        'miss': global_scores['miss'],
-        'falarm': global_scores['falarm'],
-        'confusion': global_scores['confusion'],
-    }
+    print(global_scores)
+
+    return global_scores
